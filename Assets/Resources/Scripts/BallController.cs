7810c56cfd922ea57b893415f2391fa8ec819292
WaniFi <chaos0824@naver.com>
2020-02-25 15:40:58 +0900

    Player Ball 이동을 관리하는 BallController 스크립트 추가

---
+++ Assets/Resources/Scripts/BallController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject DirectionUI;
+    [SerializeField]
+    private Rigidbody BallBody;
+    [SerializeField]
+    private Camera MainCamera;
+    [SerializeField]
+    private float BallForce;
+
+    private bool IsCharConnect;
+    private Transform DirectionTrans;
+
+    private Ray MouseRay;
+    private Vector3 DestPos;
+    private RaycastHit HitInfo;
+    private IEnumerator DirectionCoroutine;
+    
+    // Start is called before the first frame update
+    void Awake()
+    {
+        DirectionCoroutine = DirectionRotate();
+        DirectionTrans = DirectionUI.GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveBallByMouse();
+    }
+
+    private void MoveBallByMouse()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            DirectionUI.SetActive(true);
+            StartCoroutine(DirectionCoroutine);
+        }
+        if(Input.GetMouseButton(0))
+        {
+            MouseRay = MainCamera.ScreenPointToRay(Input.mousePosition);
+            if(Physics.Raycast(MouseRay.origin, MouseRay.direction, out HitInfo, 100f))
+            {
+                if (HitInfo.transform.tag.Equals("Ground"))
+                    DestPos = HitInfo.point;
+            }
+        }
+
+        if(Input.GetMouseButtonUp(0))
+        {
+            DirectionUI.SetActive(false);
+            StopCoroutine(DirectionCoroutine);
+            BallBody.velocity = (DestPos - transform.position).normalized * BallForce;
+        }
+            
+    }
+
+    private IEnumerator DirectionRotate()
+    {
+        while(true)
+        {
+            Vector3 _xz = DestPos;
+            _xz.y = 1f;
+
+            transform.LookAt(_xz);
+            yield return null;
+        }
+    }
+}
