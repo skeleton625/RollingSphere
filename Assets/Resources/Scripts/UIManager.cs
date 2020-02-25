405dce477f22c187d8a65900f11e23c954c64bf6
WaniFi <chaos0824@naver.com>
2020-02-26 00:16:20 +0900

    게임의 전반적인 UI를 관리하는 스크립트 추가

---
+++ Assets/Resources/Scripts/UIManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField]
+    private Text ScoreText;
+
+    private int ScoreNum;
+    public static UIManager instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public void PlusScore()
+    {
+        ++ScoreNum;
+        ScoreText.text = ScoreNum.ToString();
+    }
+}
