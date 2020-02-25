dedebbe71d87fc9ec4efba3a9b02cf8fbad6402c
WaniFi <chaos0824@naver.com>
2020-02-26 00:14:40 +0900

    Block의 충돌 시의 처리를 관리하는 스크립트 추가

---
+++ Assets/Resources/Scripts/BlockCollider.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockCollider : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject SplitBlockModel;
+    private GameObject PreSplitBlock;
+
+    private UIManager UManager;
+    private BlockGenerator BG;
+
+    private void Start()
+    {
+        BG = BlockGenerator.instance;
+        UManager = UIManager.instance;
+        PreSplitBlock = Instantiate(SplitBlockModel, Vector3.zero, Quaternion.identity);
+        PreSplitBlock.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag.Equals("Player"))
+        {
+            PreSplitBlock.transform.position = transform.position;
+            PreSplitBlock.SetActive(true);
+            BG.PushPreBlock(gameObject);
+            UManager.PlusScore();
+            gameObject.SetActive(false);
+        }
+    }
+}
