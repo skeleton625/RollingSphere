749a3033b74be6dc403bb3a7ed8557bd11064ef2
WaniFi <chaos0824@naver.com>
2020-02-26 00:15:59 +0900

    SplitBlock Object를 관리하는 스크립트 추가

---
+++ Assets/Resources/Scripts/SplitBlockController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplitBlockController : MonoBehaviour
+{
+    [SerializeField]
+    private int BlockCount;
+
+    private float DeActiveTime = 2f;
+    private Vector3[] DefaultPos;
+    private Transform[] Blocks;
+
+    private void Awake()
+    {
+        InitiateSplitBlockPos();
+    }
+
+    private void OnEnable()
+    {
+        StartCoroutine(SetActiveSplitBlock());
+    }
+
+    private IEnumerator SetActiveSplitBlock()
+    {
+        yield return new WaitForSeconds(DeActiveTime);
+        ReInitiateBlockPos();
+        gameObject.SetActive(false);
+    }
+
+    private void InitiateSplitBlockPos()
+    {
+        Blocks = new Transform[BlockCount];
+        DefaultPos = new Vector3[BlockCount];
+
+        for (int i = 0; i < BlockCount; i++)
+        {
+            Blocks[i] = transform.GetChild(i);
+            DefaultPos[i] = Blocks[i].transform.localPosition;
+        }
+    }
+
+    private void ReInitiateBlockPos()
+    {
+        for(int i = 0; i < BlockCount; i++)
+            Blocks[i].localPosition = DefaultPos[i];
+    }
+}
