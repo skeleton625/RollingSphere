312d343ac4cf17c24aec51142213b8ec695f8039
WaniFi <chaos0824@naver.com>
2020-02-28 23:03:29 +0900

    적 Object 관리 스크립트 추가

---
+++ Assets/Resources/Scripts/EnemyController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    private GameManager GManager;
+    private void Start()
+    {
+        GManager = GameManager.instance;
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag.Equals("Player"))
+        {
+            GManager.IsGameOver = true;
+            Destroy(collision.gameObject);
+        }
+    }
+}
