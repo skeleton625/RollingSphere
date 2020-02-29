2a4edb7df963c6e042ef65332f376033239ca73b
WaniFi <chaos0824@naver.com>
2020-02-29 12:16:34 +0900

    불필요한 코드들 제거

--- Assets/Resources/Scripts/EnemyController.cs
+++ Assets/Resources/Scripts/EnemyController.cs
@@ -4,17 +4,9 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    private GameManager GManager;
-    private void Start()
-    {
-        GManager = GameManager.instance;
-    }
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.tag.Equals("Player"))
-        {
-            GManager.IsGameOver = true;
-            Destroy(collision.gameObject);
-        }
+            collision.gameObject.SetActive(false);
     }
 }
